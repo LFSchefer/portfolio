11c62e07e68c5e5df938d9d351c11912e7b7e6dc
louis frederic schefer <lf.schefer@gmail.com>
2023-07-21 13:53:29 +0200

    start nice messes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ScrollBar from './components/ScrollBar';
 import ScrollUp from './components/ScrollUp';
+import NiceMesses from './components/NiceMesses';
 
 function App() {
   return (
     <div className="App">
       <ScrollBar/>
+      <NiceMesses/>
       <ScrollUp/>
     </div>
   );
---
+++ src/components/NiceMesses.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./NiceMesses.css";
+
+export default function NiceMesses() {
+
+  return (
+    <>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/_8g3QtiEXC0?start=5078" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </>
+  )
+
+}
