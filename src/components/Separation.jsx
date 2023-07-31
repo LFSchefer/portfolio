61a4de3b1a61068903918a664b8c7024feb0bd3f
louis frederic schefer <lf.schefer@gmail.com>
2023-07-31 08:58:50 +0200

    component work

--- src/components/Separation.jsx
+++ src/components/Separation.jsx
@@ -9,8 +9,12 @@ export default function Separation() {
    setScroolY(window.scrollY)
   }
 
-  console.log(scrollY)
-  console.log(window.innerWidth)
+  let overlap = scrollY / window.innerWidth
+
+  console.log(4*overlap)
+
+  // console.log(scrollY)
+  // console.log(window.innerWidth)
 
   React.useEffect(() => {
     function watchScroll() {
@@ -23,19 +27,19 @@ export default function Separation() {
   },[])
 
   const line1 = <div className="line1"
-  style={{right: 500 + scrollY}}>
+  style={{right: 500 + scrollY * 1 < window.innerWidth ? scrollY * 1 : scrollY * 1 - ((Math.floor(1 * overlap) * window.innerWidth) + 214) }}>
   </div>
 
   const line2 = <div className="line2"
-  style={{right: scrollY * 2}}>
+  style={{right: scrollY * 2  < window.innerWidth ? scrollY * 2 : scrollY * 2 - ((Math.floor(2 * overlap) * window.innerWidth) + 214) }}>
   </div>
 
   const line3 = <div className="line3"
-  style={{right: scrollY * 1.5}}>
+  style={{right: 200 + scrollY * 1.5 < window.innerWidth ? scrollY * 1.5 : scrollY * 1.5 - ((Math.floor(1.5 * overlap) * window.innerWidth) + 214) }}>
   </div>
 
 const line4 = <div className="line4"
-style={{right: scrollY * 3 < window.innerWidth ? scrollY * 3 : scrollY * 3 - (window.innerWidth + 214) }}>
+style={{right: scrollY * 3 < window.innerWidth ? scrollY * 3 : scrollY * 3 - ((Math.floor(3 * overlap) * window.innerWidth) + 214) }}>
 </div>
 
   return (
